cf3a1e5deccf00b926187670eaf90457b884daeb
Max Ribbans <ribbans.max@gmail.com>
2020-10-07 15:06:07 -0400

    Uncomment name and opening_time tests

--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -11,14 +11,14 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_has_opening_time
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal '10:00', restaurant.opening_time
   end
 
   def test_it_has_name
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal 'Fuel Cafe', restaurant.name
